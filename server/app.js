74361d21e73dc5afdb3e7db18ac8b40e15ddcf63
Terry Yun <nooheat1228@gmail.com>
2017-07-30 06:49:44 +0900

    app.js 정상화

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ let express = require('express');
 let bodyParser = require('body-parser');
 let sessionParser = require('express-session');
 let morgan = require('morgan');
-let database = require('./database');
+let database = require('./database/');
 
 let app = express();
 let router = require('./routes');
@@ -23,28 +23,7 @@ app.use(sessionParser({
 app.use(bodyParser.json());
 app.use('/', router);
 
-app.route('/foods').get(function (req, res) {
-    let response = [{
-            "name": "카라멜 프라푸치노",
-            "category": 7,
-            "place": "스타벅스",
-            "location": "강남역 4번출구 앞",
-            "date": "2017-07-29",
-            "stars": 3,
-            "recommend": false
-        },
-        {
-            "name": "치즈 떡볶이",
-            "category": 6,
-            "place": "죠스떡볶이",
-            "location": "은행동",
-            "date": "2017-07-29",
-            "stars": 5,
-            "recommend": true
-        }
-    ];
-    res.status(200).json(response);
-})
+
 app.listen(app.get('port'), function () {
     console.log("Started :: " + app.get('port'));
     database.connect(app);
